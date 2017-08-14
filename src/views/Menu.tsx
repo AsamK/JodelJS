8c154c2e686ef11b7e1743f8abffa8d1139e6922
AsamK <asamk@gmx.de>
2017-08-14 22:12:20 +0200

    Make menu opening more robust

--- src/views/Menu.tsx
+++ src/views/Menu.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Component} from 'react';
 import {connect, Dispatch} from 'react-redux';
 import {showNotifications, showSettings} from '../redux/actions';
 import {IJodelAppStore} from '../redux/reducers';
@@ -11,40 +12,59 @@ interface IMenuComponentProps {
     unreadNotifications: number;
 }
 
-function MenuComponent({showSettings, showNotifications, unreadNotifications}: IMenuComponentProps) {
-    return (
-        <div className="menu" tabIndex={99999999}>
-            <ul className="menuContent">
-                <li className="menuEntry">
-                    <SectionLink section="mine"/>
-                </li>
-                <li className="menuEntry">
-                    <SectionLink section="mineReplies"/>
-                </li>
-                <li className="menuEntry">
-                    <SectionLink section="mineVotes"/>
-                </li>
-                <li className="menuEntry">
-                    <SectionLink section="minePinned"/>
-                </li>
-                <li className="menuEntry">
-                    <div className="sectionLink" onClick={showNotifications}>
-                        Benachrichtigungen {unreadNotifications === 0 ? '' : `(${unreadNotifications})`}
-                    </div>
-                </li>
-                <li className="menuEntry">
-                    <div className="sectionLink" onClick={showSettings}>
-                        Einstellungen
-                    </div>
-                </li>
-            </ul>
-        </div>
-    );
+interface IMenuComponentState {
+    menuOpen: boolean;
+}
+
+class MenuComponent extends Component<IMenuComponentProps, IMenuComponentState> {
+    constructor(props: IMenuComponentProps) {
+        super(props);
+        this.state = {
+            menuOpen: false,
+        };
+    }
+
+    render() {
+        const {unreadNotifications, showNotifications, showSettings} = this.props;
+        return (
+            <div className="menu"
+                 tabIndex={99999999}
+                 onClick={() => this.setState({menuOpen: !this.state.menuOpen})}
+            >
+                {!this.state.menuOpen ? '' :
+                    <ul className="menuContent">
+                        <li className="menuEntry">
+                            <SectionLink section="mine"/>
+                        </li>
+                        <li className="menuEntry">
+                            <SectionLink section="mineReplies"/>
+                        </li>
+                        <li className="menuEntry">
+                            <SectionLink section="mineVotes"/>
+                        </li>
+                        <li className="menuEntry">
+                            <SectionLink section="minePinned"/>
+                        </li>
+                        <li className="menuEntry">
+                            <div className="sectionLink" onClick={showNotifications}>
+                                Benachrichtigungen {unreadNotifications === 0 ? '' : `(${unreadNotifications})`}
+                            </div>
+                        </li>
+                        <li className="menuEntry">
+                            <div className="sectionLink" onClick={showSettings}>
+                                Einstellungen
+                            </div>
+                        </li>
+                    </ul>
+                }
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state: IJodelAppStore) => {
     return {
-        unreadNotifications: state.entities.notifications.filter(n => !n.read).length
+        unreadNotifications: state.entities.notifications.filter(n => !n.read).length,
     };
 };
 

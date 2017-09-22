13140ef896f253a0be697d1adc2b5abecae5625b
AsamK <asamk@gmx.de>
2017-09-23 00:40:04 +0200

    Implement link parsing in stickies

--- src/views/Message.tsx
+++ src/views/Message.tsx
@@ -10,27 +10,43 @@ export interface IMessageProps {
 export default class Message extends Component<IMessageProps> {
     public render() {
         const {message, onAtClick, onHashtagClick} = this.props;
-        const linkReg = /([^#@]*)([@#])([^\s#@:;.,]*)/mg;
+        const linkReg = /([^#@]*)([@#])([^\s#@:;.,]*)|([^[]*)\[([^[\]]*)\]\(([^()]*)\)/mg;
         let previousIndex = 0;
         const messageParts = [];
         while (true) {
             const regResult = linkReg.exec(message);
+
             if (regResult === null) {
                 messageParts.push(message.substring(previousIndex));
                 break;
             }
-            messageParts.push(regResult[1]);
-            if (regResult[2] === '@') {
-                const channel = regResult[3];
-                messageParts.push(<a key={linkReg.lastIndex} className="channel-link"
-                                     onClick={e => onAtClick(e, channel)}>@{channel}</a>);
-            } else if (regResult[2] === '#') {
-                const hashtag = regResult[3];
-                messageParts.push(<a key={linkReg.lastIndex} className="hashtag"
-                                     onClick={e => onHashtagClick(e, hashtag)}>#{hashtag}</a>);
+            if (regResult[1] !== undefined) {
+                messageParts.push(regResult[1]);
+                if (regResult[2] === '@') {
+                    const channel = regResult[3];
+                    messageParts.push(<a key={linkReg.lastIndex} className="channel-link"
+                                         onClick={e => onAtClick(e, channel)}>@{channel}</a>);
+                } else if (regResult[2] === '#') {
+                    const hashtag = regResult[3];
+                    messageParts.push(<a key={linkReg.lastIndex} className="hashtag"
+                                         onClick={e => onHashtagClick(e, hashtag)}>#{hashtag}</a>);
+                }
+            } else if (regResult[4] !== undefined) {
+                messageParts.push(regResult[4]);
+                const linkLabel = regResult[5];
+                const linkTarget = regResult[6];
+                messageParts.push(
+                    <a className="sticky-link"
+                       key={linkLabel + linkTarget}
+                       href={linkTarget}
+                    >
+                        {linkLabel}
+                    </a>,
+                );
             }
             previousIndex = linkReg.lastIndex;
         }
+
         return (
             <div className="postMessage">{messageParts}</div>
         );

d953ca5600a80212206d94401357faafcefa7f0a
AsamK <asamk@gmx.de>
2018-02-10 23:47:37 +0100

    Add more user types

--- src/enums/UserType.ts
+++ src/enums/UserType.ts
@@ -1,5 +1,8 @@
 export const enum UserType {
     STUDENT = 'student', // Student
     APPRENTICE = 'apprentice', // Azubi
+    EMPLOYEE = 'employee',
+    HIGH_SCHOOL = 'high_school', // Schüler
     HIGH_SCHOOL_GRADUATE = 'high_school_graduate', // Abiturient
+    OTHER = 'other',
 }

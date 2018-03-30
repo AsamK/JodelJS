1a57980b548d06dcff561fd7e1f0edd18d82e77f
AsamK <asamk@gmx.de>
2018-03-30 10:39:56 +0200

    Update to webpack 4.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const isProduction = (process.argv.indexOf('-p') !== -1);
-
-const extractText = new ExtractTextPlugin({
-    filename: '[name].[contenthash].css',
-    disable: !isProduction
-});
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -50,48 +45,36 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: extractText.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            // https://github.com/webpack-contrib/css-loader#importloaders
-                            importLoaders: 0
-                        }
-                    }, {
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
                         loader: 'postcss-loader',
                         options: {
                             plugins: (loader) => [
                                 require('autoprefixer')(),
                             ]
                         }
-                    }],
-                    // use style-loader in development
-                    fallback: 'style-loader'
-                })
+                    },
+                ],
             },
             {
                 test: /\.less$/,
-                use: extractText.extract({
-                    //resolve-url-loader may be chained before less-loader if necessary
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            // https://github.com/webpack-contrib/css-loader#importloaders
-                            importLoaders: 0
-                        }
-                    }, {
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
                         loader: 'postcss-loader',
                         options: {
                             plugins: (loader) => [
                                 require('autoprefixer')(),
                             ]
                         }
-                    }, {
+                    },
+                    {
                         loader: 'less-loader'
-                    }],
-                    // use style-loader in development
-                    fallback: 'style-loader'
-                })
+                    }
+                ],
             },
             {
                 test: /\.svg$/,
@@ -99,23 +82,25 @@ module.exports = {
                     loader: 'svg-url-loader',
                     options: {}
                 }
-            }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
         ]
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        extractText,
+        new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[name].[contenthash].css"
+        }),
         new webpack.HashedModuleIdsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: function (module) {
-                return module.context && module.context.indexOf('node_modules') !== -1;
-            }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'runtime',
-            minChunks: Infinity
-        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         }),
@@ -123,6 +108,21 @@ module.exports = {
             'src/manifest.webmanifest'
         ])
     ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        runtimeChunk: true,
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+
 
     devServer: {
         inline: true,

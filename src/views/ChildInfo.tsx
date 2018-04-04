41bf4047057c09d320ceab27ce29461884eacd6d
AsamK <asamk@gmx.de>
2018-04-04 21:50:09 +0200

    Update packages

--- src/views/ChildInfo.tsx
+++ src/views/ChildInfo.tsx
@@ -5,7 +5,7 @@ export interface IChildInfoProps {
 }
 
 const ChildInfo = ({child_count}: IChildInfoProps) => (
-    <div className="childInfo" style={{visibility: child_count > 0 ? null : 'hidden'}}>
+    <div className="childInfo" style={{visibility: child_count > 0 ? undefined : 'hidden'}}>
         {child_count} Kommentare
     </div>
 );

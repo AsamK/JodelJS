1274cefe42513e6b70147c855c670927aa3400a1
AsamK <asamk@gmx.de>
2018-07-25 22:52:54 +0200

    Split main parts of the app

--- src/views/Jodel.tsx
+++ src/views/Jodel.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import loadable from 'react-loadable';
 import {connect} from 'react-redux';
+
 import {IPost} from '../interfaces/IPost';
 import {JodelThunkDispatch} from '../interfaces/JodelThunkAction';
 import {fetchPostsIfNeeded, showChannelList, showSettings} from '../redux/actions';
@@ -14,22 +16,42 @@ import {
     getSearchVisible,
     getSettingsVisible,
 } from '../redux/selectors/view';
-import {AddPost} from './AddPost';
-import AppSettings from './AppSettings';
 import BigPicture from './BigPicture';
-import ChannelList from './ChannelList';
 import ChannelTopBar from './ChannelTopBar';
 import FirstStart from './FirstStart';
 import {HashtagTopBar} from './HashtagTopBar';
-import {NotificationList} from './NotificationList';
 import PostDetails from './PostDetails';
 import {PostListContainer} from './PostListContainer';
 import {PostTopBar} from './PostTopBar';
 import Progress from './Progress';
-import {Search} from './Search';
 import ShareLink from './ShareLink';
 import {ToastContainer} from './ToastContainer';
 import {TopBar} from './TopBar';
+
+const LoadableAddPost = loadable({
+    loader: () => import('./AddPost').then(module => module.AddPost),
+    loading: () => null,
+});
+
+const LoadableNotificationList = loadable({
+    loader: () => import('./NotificationList').then(module => module.NotificationList),
+    loading: () => null,
+});
+
+const LoadableSearch = loadable({
+    loader: () => import('./Search').then(module => module.Search),
+    loading: () => null,
+});
+
+const LoadableAppSettings = loadable({
+    loader: () => import('./AppSettings'),
+    loading: () => null,
+});
+
+const LoadableChannelList = loadable({
+    loader: () => import('./ChannelList'),
+    loading: () => null,
+});
 
 export interface IJodelProps {
     addPostVisible: boolean;
@@ -69,15 +91,15 @@ class JodelComponent extends React.Component<IJodelProps> {
             let content = null;
 
             if (this.props.addPostVisible) {
-                content = <AddPost/>;
+                content = <LoadableAddPost/>;
             } else if (this.props.notificationsVisible) {
-                content = <NotificationList/>;
+                content = <LoadableNotificationList/>;
             } else if (this.props.searchVisible) {
-                content = <Search/>;
+                content = <LoadableSearch/>;
             } else if (this.props.settingsVisible) {
-                content = <AppSettings/>;
+                content = <LoadableAppSettings/>;
             } else if (this.props.channelListVisible) {
-                content = <ChannelList/>;
+                content = <LoadableChannelList/>;
             } else if (this.props.selectedPostId != null) {
                 content = <div className="detail">
                     <PostTopBar/>

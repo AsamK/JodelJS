71341f100474ddba8d2b01342c7bb6f95b83ca5b
AsamK <asamk@gmx.de>
2018-07-29 12:00:11 +0200

    Split add post style

--- src/views/AddPost.tsx
+++ src/views/AddPost.tsx
@@ -1,15 +1,16 @@
 import classnames from 'classnames';
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-import {Color} from '../enums/Color';
-import {JodelThunkDispatch} from '../interfaces/JodelThunkAction';
-import {addPost, switchPostSection} from '../redux/actions';
-import {IJodelAppStore} from '../redux/reducers';
-import {getAddPostChannel} from '../redux/selectors/app';
-import {getSelectedPostId} from '../redux/selectors/posts';
-import {getAddPostVisible} from '../redux/selectors/view';
-import {resizePicture} from '../utils/picture.utils';
+import { Color } from '../enums/Color';
+import { JodelThunkDispatch } from '../interfaces/JodelThunkAction';
+import { addPost, switchPostSection } from '../redux/actions';
+import { IJodelAppStore } from '../redux/reducers';
+import { getAddPostChannel } from '../redux/selectors/app';
+import { getSelectedPostId } from '../redux/selectors/posts';
+import { getAddPostVisible } from '../redux/selectors/view';
+import { resizePicture } from '../utils/picture.utils';
+import './AddPost.scss';
 import ColorPicker from './ColorPicker';
 
 export interface IAddPostComponentProps {
@@ -42,10 +43,10 @@ export class AddPostComponent extends React.PureComponent<IAddPostComponentProps
     }
 
     public render() {
-        const {channel, ancestor, visible} = this.props;
+        const { channel, ancestor, visible } = this.props;
 
         return (
-            <div className={classnames('addPost', {visible})}>
+            <div className={classnames('addPost', { visible })}>
                 {ancestor === null ?
                     'Neuen Jodel schreiben' + (channel != null ? ' (Kanal: ' + channel + ')' : '')
                     :
@@ -53,19 +54,19 @@ export class AddPostComponent extends React.PureComponent<IAddPostComponentProps
                 }:
                 <form onSubmit={this.handleAddPost}>
                     <textarea maxLength={MAX_POST_CHARS} value={this.state.message} onChange={event => {
-                        this.setState({message: event.target.value});
+                        this.setState({ message: event.target.value });
                         sessionStorage.setItem('messageDraft', event.target.value);
-                    }}/>
+                    }} />
                     Noch {MAX_POST_CHARS - this.state.message.length} Zeichen
                     <div className="image">
                         Bild Jodeln:
-                        <input type="file" accept="image/*" onChange={this.handleChangeImage}/>
+                        <input type="file" accept="image/*" onChange={this.handleChangeImage} />
                         {this.state.imageUrl && this.state.image ?
-                            <img src={this.state.imageUrl} alt={this.state.image.name}/> : ''}
+                            <img src={this.state.imageUrl} alt={this.state.image.name} /> : ''}
                     </div>
                     {ancestor === null ? <ColorPicker color={this.state.color} onChange={color => {
-                        this.setState({color});
-                    }}/> : ''}
+                        this.setState({ color });
+                    }} /> : ''}
                     <button type="submit">
                         Senden
                     </button>
@@ -87,24 +88,24 @@ export class AddPostComponent extends React.PureComponent<IAddPostComponentProps
         }
         const input: HTMLInputElement = event.target;
         if (!input.files || input.files.length === 0) {
-            this.setState({image: undefined, imageUrl: undefined});
+            this.setState({ image: undefined, imageUrl: undefined });
             return;
         }
-        this.setState({image: input.files[0]});
+        this.setState({ image: input.files[0] });
         if ('URL' in window && 'createObjectURL' in window.URL) {
             const url = window.URL.createObjectURL(input.files[0]);
-            this.setState({imageUrl: url});
+            this.setState({ imageUrl: url });
         }
     };
 
     private resetForm = (form: HTMLFormElement) => {
-        this.setState({message: '', image: undefined, imageUrl: undefined});
+        this.setState({ message: '', image: undefined, imageUrl: undefined });
         form.reset();
         sessionStorage.removeItem('messageDraft');
     };
 
     private handleAddPost = (event: React.FormEvent<HTMLFormElement>) => {
-        const {channel, ancestor} = this.props;
+        const { channel, ancestor } = this.props;
         event.preventDefault();
         if ((this.state.message.trim() === '' && this.state.image === null) ||
             !(event.target instanceof HTMLFormElement)) {
@@ -124,7 +125,7 @@ export class AddPostComponent extends React.PureComponent<IAddPostComponentProps
     };
 
     private sendAddPost(message: string, encodedImage: string | undefined, channel: string | undefined,
-                        ancestor: string | undefined, color: Color | undefined, form: HTMLFormElement) {
+        ancestor: string | undefined, color: Color | undefined, form: HTMLFormElement) {
         this.props.dispatch(addPost(message, encodedImage, channel, ancestor, color)).then(
             section => {
                 this.resetForm(form);

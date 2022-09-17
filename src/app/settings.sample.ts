2d7640d30e4a483d2996ea72f2fc5e6fbe7115ed
AsamK <asamk@gmx.de>
2022-09-17 17:02:22 +0200

    Update secret

--- src/app/settings.sample.ts
+++ src/app/settings.sample.ts
@@ -2,13 +2,13 @@ import type { ISettings } from '../interfaces/ISettings';
 
 const Settings: ISettings = {
     // API Server and Path
-    API_SERVER: 'https://api.go-tellm.com/api',
+    API_SERVER: 'https://api.jodelapis.com/api',
 
     // Android client id
     CLIENT_ID: '81e8a76e-1e02-4d17-9ba0-8a7020261b26',
 
     // Client type for signed requests
-    CLIENT_TYPE: 'android_5.109.3',
+    CLIENT_TYPE: 'android_8.4.0',
 
     // Default location, if browser location is not available
     DEFAULT_LOCATION: undefined,
@@ -20,7 +20,7 @@ const Settings: ISettings = {
     GCM_RECEIVE_HELPER_URL: '',
 
     // Key for signed requests
-    KEY: 'bMUGjmkdcQgMhWNwbgcSwvzGsTZLugkAsoMBBxpf',
+    KEY: 'GNyUrEmBdEkihJOIoUTXbCQmBpDSxfFNGCuaWAUH',
 
     // Colors for posts, the Server prevents other colors
     POST_COLORS: ['06A3CB', 'DD5F5F', 'FFBA00', 'FF9908', '8ABDB0', '9EC41C'],
